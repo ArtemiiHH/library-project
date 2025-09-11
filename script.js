f14ac8096b69c10577c6dd2b702c706cdafcdbee
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-09-11 19:27:51 +0300

    Make X and Cancel buttons close modal

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const myLibrary = ['book1', 'book2'];
 const addBtn = document.querySelector('.add-btn');
 const modal = document.querySelector('#modal-container');
 modal.classList.add('hidden');
+const exitBtn = document.querySelector('.x-btn');
+const calcelBtn = document.querySelector('.close-btn');
 
 function Book(title, author, pages, hasRead) {
     this.id = crypto.randomUUID();
@@ -15,11 +17,21 @@ function Book(title, author, pages, hasRead) {
 function addBookToLibrary(title, author, pages, hasRead) {
     const newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook);
-    console.log(myLibrary);
+    return myLibrary;
 };
 
 
-
+// open modal when Add button pressed
 addBtn.addEventListener('click', () => {
     modal.classList.remove('hidden');
+});
+
+// Close modal when X pressed
+exitBtn.addEventListener('click', () => {
+    modal.classList.toggle('hidden');
+});
+
+// Close modal when Close pressed
+calcelBtn.addEventListener('click', () => {
+    modal.classList.toggle('hidden');
 });
