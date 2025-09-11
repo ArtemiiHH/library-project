2558899031d8a0c87923ea5579fe361cd271551e
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-09-11 14:22:35 +0300

    Add basic JS functions

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book() {
+
+};
+
+function addBookToLibrary(title, author, pages, hasRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+};
